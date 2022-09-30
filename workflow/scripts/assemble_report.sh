377672b18262f3ab9871705dd1d6b0256e65172d
Gray <gr104@wellesley.edu>
2022-09-30 17:01:28 -0400

    refined the report, can't quite get running a second line of bash code to work

--- workflow/scripts/assemble_report.sh
+++ workflow/scripts/assemble_report.sh
@@ -5,37 +5,44 @@
 fwd_list=$1
 bwd_list=$2
 method=$3
-#name=$4
+name=$4
 
 #report inputs
 
 #header
 
-echo "                                     "
-echo "       ASSEMBLE REPORT FOR RUN       "
-#echo "            $name         "
-echo "_____________________________________"
-echo "                                     "
+echo " "
+echo "                     ASSEMBLE REPORT FOR RUN"
+echo "                          $name"
+echo "_________________________________________________________________"
+echo " "
 
 
 #method
-echo "ASSEBLY METHOD: $method" 
-echo "                                     "
+echo "ASSEMBLY METHOD:"
+echo "   $method"
+echo " "
 
 
 #inputs
 echo "FORWARD READS:"
 for file in $fwd_list
 do
-	echo "   $file"
+	echo "   $file" | fold -w 65
 done
-echo "                                     "
-echo "BACKWARD READS:                      "
+echo " "
+echo "BACKWARD READS:"
 for file in $bwd_list
 do
-	echo "   $file"
+	echo "   $file" | fold -w 65
 done
+echo " "
+echo "_________________________________________________________________"
+echo " "
 
+#report meat
 
-
+echo "TOTAL ASSEMBLY TIME:                TIME PER READ:"
+echo "   time here                           time here"
+echo " "
 #
