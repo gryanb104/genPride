648a6da426344e89e29d6e939a4c549d6182001f
Gray <gr104@wellesley.edu>
2022-10-04 18:27:54 -0400

    changed genPridePush to also track deletions added a functioning version of spades got some slurm stuff running, still an issue with passing in parameters

--- workflow/scripts/assemble_meat.sh
+++ workflow/scripts/assemble_meat.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#SBATCH -J metaSpades_${14}	#Job name
+#SBATCH	-n ${9}			#number of cpus
+#SBATCH -N ${8}			#number of nodes
+#SBATCH --mem=${10}
+#SBATCH -t ${11}		#runtime
+#SBATCH -p ${12}		#partition
+#SBATCH --${13}			#exclusive need?
 
 #reassign variable names
 
@@ -6,10 +13,32 @@ fwd_list=$1
 bwd_list=$2
 meth=$3
 ass_thresh=$4
+threads=$5
+spm=$6
+spades_path=$7
+nodes=$8
+cpus=$9
+memory=$10
+runtime=$11
+partition=$12
+ex_need=$13
+name=$14
+
+#reset timer
+SECONDS=0
+
+if [[ "$meth" == "metaspades" ]]
+then
+	${spades_path}/spades.py \
+		-o "results/assembled_seqs" \
+		--meta \
+		-1 ${1} \
+		-2 ${2} \
+		-t ${threads} -m ${spm}
+else
+	echo "Assembly method $meth not valid"
+fi
 
 
-echo $fwd_list
-echo $bwd_list
-echo $meth
-echo $ass_thresh
+
 
--- workflow/scripts/assembly_meat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#set input names
-
-fwd_list=$1
-#bwd_list=$2
-#meth=$3
-
-echo "this is a test"
