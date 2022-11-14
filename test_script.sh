346eae5acf12ba3a6c9075d0238aae5e6298b89d
Gray <gr104@wellesley.edu>
2022-11-14 11:28:06 -0500

    got spades working in test_script

--- test_script.sh
+++ test_script.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=GP_test             #job name
+#SBATCH --nodes=1                       #number of nodes to use
+#SBATCH --ntasks=20                     #number of cpus to use
+#SBATCH --mem=250000                    #maximum memory
+#SBATCH --time=2-00:00:00               #maximum time to run
+#SBATCH --partition=sched_mit_chisholm  #partition name
+#SBATCH --exclusive                     #exclusive use of node
+
 fwd="/nobackup1/billerlab/gray/data_GP/subset_fwd.fastq.gz"
 bwd="/nobackup1/billerlab/gray/data_GP/subset_bwd.fastq.gz"
 meth="metaspades"
