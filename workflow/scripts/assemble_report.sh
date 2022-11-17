dba4db618717a40e713228f2e18c4d8b219f7e4d
Gray <gr104@wellesley.edu>
2022-11-17 18:38:08 -0500

    added orf report functionality, operated through snakefile

--- workflow/scripts/assemble_report.sh
+++ workflow/scripts/assemble_report.sh
@@ -8,27 +8,24 @@ method=$3
 name=$4
 slurm_ass=$5
 ass_contigs=$6
+prod_meth=$7
+slurm_orf=$8
 
 #report inputs
 
 #header
 
 echo " "
-echo "                     ASSEMBLE REPORT FOR RUN"
-echo "                          $name"
+echo "                         REPORT FOR RUN"
+echo "                           $name"
 echo "_________________________________________________________________"
 echo " "
-
-#inputs
-
+echo "                           ASSEMBLY"
+echo " "
 echo "ASSEMBLY METHOD GP : $method"
 echo "FORWARD READS FILE : ${fwd_list##*/}"
 echo "REVERSE READS FILE : ${bwd_list##*/}"
 echo " "
-echo "_________________________________________________________________"
-echo " "
-
-#report meat
 
 seqs_start_fwd=$(zcat $fwd_list | echo $((`wc -l`/4)))
 contigs_end=$(grep ">" $ass_contigs | wc -l)
@@ -39,6 +36,7 @@ echo "  N OUTPUT CONTIGS : $contigs_end"
 echo " "
 
 sec=$(tail -n 1 $slurm_ass)
+sec_ASS=$sec
 min=$(($sec / 60))
 only_sec=$(($sec - ($min * 60)))
 hour=$(($sec / 3600))
@@ -52,3 +50,25 @@ sec_c=$(printf "%f\n" $((10**6 * $sec/$contigs_end))e-6)
 
 echo -n "  T PER INPUT READ : $sec_f"; echo "s"
 echo -n "  T PER OUT CONTIG : $sec_c"; echo "s"
+
+echo " "
+echo "_________________________________________________________________"
+echo " "
+echo "                       PROTEIN SEQUENCING"
+echo " "
+echo "PROTEIN SEQ METHOD : $prod_meth"
+echo " "
+echo "PROTEINS SEQUENCED : $contigs_end"
+echo " "
+
+sec=$(tail -n 1 $slurm_orf)
+min=$(($sec / 60))
+only_sec=$(($sec - ($min * 60)))
+hour=$(($sec / 3600))
+only_min=$(($min - ($hour * 60)))
+
+echo "PROTEIN SEQUENCE TIME"
+echo -n "  TOTAL TIME ELAPS : $hour"; echo -n "h $only_min"; echo -n "m $only_sec"; echo "s"
+sec_c=$(printf "%f\n" $((10**6 * $sec/$contigs_end))e-6)
+echo -n "  TIME PER PROTEIN : $sec_c"; echo "s"
+echo " "
