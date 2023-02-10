9069e1c2a377fcd9e279715e6a3da44200d853d4
Gray <gr104@wellesley.edu>
2023-02-10 13:28:55 -0500

    updating before adding GORG capabilities

--- workflow/scripts/find_orfs.sh
+++ workflow/scripts/find_orfs.sh
@@ -9,14 +9,27 @@ start=$SECONDS
 
 mkdir results/protein_seqs
 
-${prod_path}/prodigal \
-	-i ${ass_contigs} \
-	-o results/protein_seqs/prodigal_output.gff \
-	-f gff \
-	-a results/protein_seqs/protein_translations.faa \
-	-p ${prod_meth} \
-	-c
-
+if [[ "$prod_meth" == "meta_comp" ]]
+then
+	${prod_path}/prodigal \
+		-i ${ass_contigs} \
+		-o results/protein_seqs/prodigal_output.gff \
+		-f gff \
+		-a results/protein_seqs/protein_translations.faa \
+		-p "meta" \
+		-c
+elif [[ "$prod_meth" == "meta" ]]
+then
+        ${prod_path}/prodigal \
+                -i ${ass_contigs} \
+                -o results/protein_seqs/prodigal_output.gff \
+                -f gff \
+                -a results/protein_seqs/protein_translations.faa \
+                -p "meta" \
+else
+	echo "Prodigal method $prod_meth not valid"
+fi
+              
 duration=$(( SECONDS - start ))
 
 echo "time(sec):"; echo $duration
