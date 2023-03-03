9a3f01ef919c3c51594729c18ccdd1287ebdf0bd
biller <biller@eofe7.mit.edu>
2023-03-03 15:07:27 -0500

    gorg_classifier timer added

--- workflow/scripts/gorg_classifier.sh
+++ workflow/scripts/gorg_classifier.sh
@@ -6,11 +6,11 @@ gorg_path=$1
 contigs=$2
 outdir=results/gorg_classification
 
+start=$SECONDS
+
 #${gorg_path}/nextflow run BigelowLab/gorg-classifier -profile docker --seqs $contigs --outdir $outdir
 
-echo $gorg_path
-echo $contigs
-echo $outdir
-
 echo "test" > results/gorg_classification/gorg.out
+echo "time(sec):"
+echo $(( SECONDS - start))
 
