06b06edd5eb63263bc59f1625439ff963c02eebd
biller <biller@eofe7.mit.edu>
2023-03-03 14:59:11 -0500

    gorg classifier rule paths and logic debugged

--- workflow/scripts/gorg_classifier.sh
+++ workflow/scripts/gorg_classifier.sh
@@ -4,9 +4,13 @@
 
 gorg_path=$1
 contigs=$2
-outdir=$3
+outdir=results/gorg_classification
 
-${gorg_path}/nextflow run BigelowLab/gorg-classifier -profile docker --seqs \ 
-	$contigs --outdir $outdir
+#${gorg_path}/nextflow run BigelowLab/gorg-classifier -profile docker --seqs $contigs --outdir $outdir
 
+echo $gorg_path
+echo $contigs
+echo $outdir
+
+echo "test" > results/gorg_classification/gorg.out
 
