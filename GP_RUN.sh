ac840ea742ac0d3777749eaaa0d167330d7a8ae3
gryanb104 <gr104@wellesley.edu>
2023-09-28 19:06:26 -0400

    coverage sorta working... ish r Please enter the commit message for your changes. Lines starting

--- GP_RUN.sh
+++ GP_RUN.sh
@@ -2,16 +2,16 @@
 #SBATCH --job-name=sbst_cvrg_test	#job name
 #SBATCH --nodes=1			#number of nodes to use
 #SBATCH --ntasks=20			#number of cpus to use
-#SBATCH --mem=250000			#maximum memory
+#SBATCH --mem=2500 #00			#maximum memory
 #SBATCH --time=0-08:00:00		#maximum time to run
 #SBATCH --partition=sched_mit_chisholm	#partition name
+
+
+
 #SBATCH --exclusive			#exclusive use of node
 
 source ~/.bashrc
 conda activate snakeGenPride
 snakemake --unlock
 
-doc_inp=$(python3 workflow/scripts/getDocInp.py)
-
-#snakemake --cores 16 --config final_output=$doc_inp $doc_inp
 snakemake --cores 16
--- workflow/scripts/coverage.sh
+++ workflow/scripts/coverage.sh
@@ -3,22 +3,105 @@
 paired=$1
 covr_inp=$2
 sample=$3
-output_file=$4
+output_directory=$4
 coverage_method=$5
 reads_fwd=$6
 reads_rev=$7
+output_file=$8
 
-source workflow/rules/coverage.smk
+echo "__________________________________________________"
+echo ""
+echo "COVERAGE REPORT FOR SAMPLE ${sample}"
+echo ""
+echo "INPUTS:"
+echo -n "  paired: "
+echo $paired
+echo -n "  sample: "
+echo $sample
+echo -n "  output directory: "
+echo $output_directory
+echo -n "  coverage method: "
+echo $coverage_method
+echo -n "  forward reads: "
+echo $reads_fwd
+echo -n "  reverse reads: "
+echo $reads_rev
+echo ""
+echo "METHODS: "
 
 #set variables
-
 coverage_method_first="$(echo $coverage_method | grep -o '^[^:]*')"
+echo "  METHOD ONE: ${coverage_method_first}"
 coverage_method_second="$(echo $coverage_method | awk -F ':' '{print $2}')"
+echo "  METHOD TWO: ${coverage_method_second}"
 coverage_method_third="$(echo $coverage_method | awk -F ':' '{print $3}')"
+echo "  METHOD THREE: ${coverage_method_third}"
 coverage_method_fourth="$(echo $coverage_method | awk -F ':' '{print $4}')"
+echo "  METHOD FOUR: ${coverage_method_fourth}"
+echo ""
+echo "COVERAGE LOG:"
 
-if [[ $coverage_method == "none" ]]; then 
-	cvr_is_none $sample $output_file
+if [[ $coverage_method_first == "none"  ]] || [[ $coverage_method_first == "None" ]] || [[ $coverage_method_first == "NONE" ]]; then 
+	echo "  NO COVERAGE CALCULATED FOR SAMPLE ${sample}"
+	mkdir $output_directory
+        echo "NULL" > $output_file
+elif [[ $coverage_method_first == "coverM-from-bam" ]]; then
+	#sort the bam files
+	samtools sort $reads_rev -o ${reads_rev}_sorted
+	samtools sort $reads_fwd -o ${reads_fwd}_sorted
+
+	#set coverM vars
+	coverM_algorithm=$coverage_method_second
+	cM_methods=$coverage_method_third
+	cM_coverage_mapper=$coverage_method_fourth
+
+	#set defaults and print
+	echo "  USING coverM TO CALCULATE COVERAGE FOR SAMPLE ${sample} FROM PREXISTING BAM FILES ${reads_fwd} AND ${reads_rev}"
+
+	##for no coverM algorithm provided
+	if [[ $coverM_algorithm == "" ]]; then
+		echo "  NO SECOND COVERAGE METHOD PROVIDED. USING coverM genome AS DEFAULT"
+		coverM_algorithm="genome"
+
+	##for coverM genome
+	elif [[ $coverM_algorithm == "genome" ]]; then
+		echo "  USING coverM genome TO CALCULATE COVERAGE"
+
+		###mapper: print and set default
+		if [[ $cM_coverage_mapper != "" ]]; then
+			echo "  coverM MAPPER ARGUMENT $cM_coverage_mapper IGNORED FOR COVERAGE CALCULATION FROM EXISTING BAM FILES"
+		fi
+
+		###coverM method: print and set default
+		if [[ $cM_methods == "" ]]; then
+			echo "  USING DEFAULT coverM genome METHOD covered_bases"
+			cM_methods="covered_bases"
+		else
+			echo "  USING coverM genome METHOD $cM_methods"
+		fi
+
+	##for coverM make
+	elif [[ $coverM_algorithm == "make" ]]; then
+		echo "  CANNOT USE coverM make ON EXISTING BAM FILES. SEE DOCUMENTATION FOR VALID coverM METHODS."
+
+	##for nonense coverM algorithm provided
+	else
+		echo "  coverM $coverM_algorithm IS UNSUPPORTED"
+	fi
+
+	#run coverM
+
+	##for coverM genome
+	if [[ $coverM_algorithm == "genome" ]]; then
+		if [[ $paired == "NA" ]]; then
+			coverm genome --bam-files ${reads_fwd}_sorted ${reads_rev}_sorted --methods $cM_methods -t 8 -r $covr_inp -o $output_directory
+			#HERE CHANGE NAME
+		fi
+	##for nonsense coverM algorithm provided
+	else
+		echo "  coverM ${coverM_algorithm} IS UNSUPPORTED. SEE DOCUMENTATION FOR SUPPORTED coverM METHODS"
+	fi
+
 elif [[ $coverage_method_first == "coverM" ]]; then
 	
 	#set coverM vars
@@ -27,70 +110,83 @@ elif [[ $coverage_method_first == "coverM" ]]; then
 	cM_coverage_mapper=$coverage_method_fourth
 
 	#set defaults and print
-	echo ""
-	echo "USING coverM TO CALCULATE COVERAGE FOR SAMPLE ${sample}"
+	echo "  USING coverM TO CALCULATE COVERAGE FOR SAMPLE ${sample}"
 
 	##for no coverM algorithm provided
 	if [[ $coverM_algorithm == "" ]]; then
-		echo "NO SECOND COVERAGE METHOD PROVIDED. USING coverM make AS DEFAULT"
+		echo "  NO SECOND COVERAGE METHOD PROVIDED. USING coverM make AS DEFAULT"
 		coverM_algorithm="make"
 
 	##for coverM genome
 	elif [[ $coverM_algorithm == "genome" ]]; then
-		echo "USING coverM genome TO CALCULATE COVERAGE"
+		echo "  USING coverM genome TO CALCULATE COVERAGE"
 
 		###mapper: print and set default
 		if [[ $cM_coverage_mapper == "" ]]; then
-			echo "USING DEFAULT MAPPER minimap2-sr"
+			echo "  USING DEFAULT MAPPER minimap2-sr"
 			coverage_mapper="minimap2-sr"
 		else
-			echo "USING COVERAGE MAPPER $cM_coverage_mapper"; fi
+			echo "  USING COVERAGE MAPPER $cM_coverage_mapper"
+		fi
 
 		###coverM method: print and set default
 		if [[ $cM_methods == "" ]]; then
-			echo "USING DEFAULT coverM genome METHOD covered_bases"
+			echo "  USING DEFAULT coverM genome METHOD covered_bases"
 			cM_methods="covered_bases"
 		else
-			echo "USING coverM genome METHOD $cM_methods"; fi
+			echo "  USING coverM genome METHOD $cM_methods"
+		fi
 
 	##for coverM make
 	elif [[ $coverM_algorithm == "make" ]]; then
-                echo "USING coverM make TO CALCULATE COVERAGE"
+		echo "  USING coverM make TO CALCULATE COVERAGE"
 
-                ###mapper: print and set default
-                if [[ $cM_coverage_mapper == "" ]]; then
-                        echo "USING DEFAULT MAPPER minimap2-sr"
-                        coverage_mapper="minimap2-sr"
-                else
-                        echo "USING COVERAGE MAPPER $cM_coverage_mapper"; fi
+		###mapper: print and set default
+		if [[ $cM_coverage_mapper == "" ]]; then
+			echo "  USING DEFAULT MAPPER minimap2-sr"
+			coverage_mapper="minimap2-sr"
+		else
+			echo "  USING COVERAGE MAPPER $cM_coverage_mapper"
+		fi
 
 	##for nonense coverM algorithm provided
 	else
-		echo "coverM $coverM_algorithm IS UNSUPPORTED"; fi
+		echo "  coverM $coverM_algorithm IS UNSUPPORTED"
+	fi
 	
 	#run coverM
 
 	##for coverM genome
 	if [[ $coverM_algorithm == "genome" ]]; then
 		if [[ $paired == "YES" ]]; then
-			coverm genome -1 $reads_fwd -2 $reads_rev --methods $cM_methods -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_file 
+			coverm genome -1 $reads_fwd -2 $reads_rev --methods $cM_methods -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_directory
+			#HERE CHANGE NAME 
 		elif [[ $paired == "NO" ]]; then 
-			coverm genome --single $reads_fwd --methods $cM_methods -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_file; fi
+			coverm genome --single $reads_fwd --methods $cM_methods -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_directory
+			#HERE CHANGE NAME 
+		fi
 
 	##for coverM make
 	elif [[ $coverM_algorithm == "make" ]]; then
 		if [[ $paired == "YES" ]]; then
-			coverm make -1 $reads_fwd -2 $reads_rev -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_file
+			coverm make -1 $reads_fwd -2 $reads_rev -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_directory
+			cp ${output_directory}/*.bam ${output_file}
 		elif [[ $paired == "NO" ]]; then
-			coverm make --single $reads_fwd -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_file; fi
+			coverm make --single $reads_fwd -t 8 -r $covr_inp --mapper $cM_coverage_mapper -o $output_directory
+			cp ${output_directory}/*.bam ${output_file}
+		fi
 
 	##for nonsense coverM algorithm provided
 	else
-		echo "coverM ${coverM_algorithm} IS UNSUPPORTED. SEE DOCUMENTATION FOR SUPPORTED coverM METHODS"; fi
+		echo "  coverM ${coverM_algorithm} IS UNSUPPORTED. SEE DOCUMENTATION FOR SUPPORTED coverM METHODS"
+	fi
+
 else
-	echo "FIRST COVERAGE METHOD ${coverage_method_first} IS UNSUPPORTED. SEE DOCUMENTATION FOR SUPPORTED COVERAGE METHODS"; fi
+	echo "  FIRST COVERAGE METHOD ${coverage_method_first} IS UNSUPPORTED. SEE DOCUMENTATION FOR SUPPORTED COVERAGE METHODS"
+fi
 
-
+echo ""
+echo "__________________________________________________"
 
 
 
